be7ed91428d23bfaa3c65746935fca1c4509dfeb
Edwin de Jager <edwindejager@gmail.com>
2026-07-18 06:40:02 +0000

    Update homepage best deals image

--- components/home/home-action-cards.tsx
+++ components/home/home-action-cards.tsx
@@ -21,7 +21,7 @@ const links: QuickLink[] = [
   {
     href: "/topdeals",
     label: "Best Deals",
-    imageSrc: "/home-cards/best-deals-white.png",
+    imageSrc: "/home-cards/bestdeal2.png",
   },
 ];
 
